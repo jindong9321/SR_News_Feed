7cc33365a74f1ebf7eac096f76c7f80906d314be
RDMac <rdmac@RDMacui-Mac-Pro-3.local>
2015-09-25 14:37:36 +0900

    Initial Commit

---
+++ NewsFeed/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  NewsFeed
+//
+//  Created by RDMac on 2015. 9. 25..
+//  Copyright (c) 2015년 RDmac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
